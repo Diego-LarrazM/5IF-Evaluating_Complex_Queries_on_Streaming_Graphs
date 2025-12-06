fd0dd861d343286a7c6f1c074419d1ee925c5324
Diego-LarrazM <larrazmartin.diego@gmail.com>
2025-12-06 23:22:07 +0100

    op

--- streamgraph_queries/src/main/java/if5/datasystems/core/models/algebra/Operator.java
+++ streamgraph_queries/src/main/java/if5/datasystems/core/models/algebra/Operator.java
@@ -1,27 +1,27 @@
 package if5.datasystems.core.models.algebra;
 import java.util.function.Function;
-import if5.datasystems.core.models.streamingGraph.StreamingGraph;
+import if5.datasystems.core.models.streaminggraph.StreamingGraph;
 
 class Operator implements Function<StreamingGraph, StreamingGraph> {
-    private Function<StreamingGraph, StreamingGraph> func;
+    private Function<Integer, Integer> func;
 
     public Operator(Function<StreamingGraph, StreamingGraph> func) {
         this.func = func;
     }
 
     @Override
-    public StreamingGraph apply(StreamingGraph s) {
-        return func.apply(s);
+    public Integer apply(Integer x) {
+        return func.apply(x);
     }
 
     // In-place (before o operator)
     public void composeAfter(Operator before) {
-        this.func = s -> this.func.apply(before.apply(s));
+        this.func = x -> this.func.apply(before.apply(x));
     }
 
     // In-place (operator o after)
     public void composeBefore(Operator after) {
-        this.func = s -> after.apply(this.func.apply(s));
+        this.func = x -> after.apply(this.func.apply(x));
     }
 }
 
