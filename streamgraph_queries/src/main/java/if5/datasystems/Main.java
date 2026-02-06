520cee7a86467ae822e09e4b3fad792b023f61a9
Diego-LarrazM <larrazmartin.diego@gmail.com>
2026-02-07 00:55:34 +0100

    WSCAN sliding window

--- streamgraph_queries/src/main/java/if5/datasystems/Main.java
+++ streamgraph_queries/src/main/java/if5/datasystems/Main.java
@@ -1,4 +1,5 @@
 package if5.datasystems;
+import java.time.Duration;
 import java.util.ArrayList;
 
 import if5.datasystems.core.models.aliases.Label;
@@ -8,8 +9,12 @@ import if5.datasystems.core.processors.StreamProcessor;
 public class Main {
     
     public static void main(String[] args) {
-        long windowSize = 3600*24; // 1 day window (in s for stack overflow)
-        //long windowSize = 5; // (in s for integration test)
+        long slide = Duration.ofDays(1).getSeconds(); // 1 day slide (in s for stack overflow)
+        long windowSize = Duration.ofDays(30).getSeconds(); // 5-30 day window (in s for stack overflow) 
+        
+        // long windowSize = 5; // (in s for integration test)
+        // long slide = 1; // (in s for integration test)
+
         ArrayList<Pair<Label, Label>> queries = new ArrayList<>();
 
         // a*
@@ -21,20 +26,20 @@ public class Main {
         );
 
         // a , b*
-        queries.add(
+        /*queries.add(
             new Pair<>(
                 new Label("c2q,a2q*"),
                 new Label("Q2" )
             )
-        );
+        );*/
 
         // a , b* , c*
-        queries.add(
+        /*queries.add(
             new Pair<>(
                 new Label("c2a,c2q*,a2q*"),
                 new Label("Q3" )
             )
-        );
+        );*/
 
 
         // Integration Test query
@@ -46,7 +51,7 @@ public class Main {
         );*/
 
 
-        StreamProcessor processor = new StreamProcessor(windowSize, 0, queries, 8080);
+        StreamProcessor processor = new StreamProcessor(windowSize, slide, queries, 8080);
         try {
             processor.execute("Stream Graph Processor");
         } catch (Exception e) {
